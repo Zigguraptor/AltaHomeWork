a94c1b20fe051c10628cf5cbdfcc2822b3f2ac6f
Zigguraptor <cobaltum8@gmail.com>
2023-10-13 00:47:02 +0400

    added Rgb24ImageWrapper.cs

---
+++ ImageProcessing/Rgb24ImageWrapper.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using MathMatrixKit.Matrix;
+
+namespace AltaHomeWork_1.ImageProcessing;
+
+public readonly struct Rgb24ImageWrapper : IMatrix<Rgb24>
+{
+    private readonly Image<Rgb24> _image;
+    public int Height => _image.Height;
+    public int Width => _image.Width;
+
+    public Rgb24 this[int x, int y]
+    {
+        get => _image[x, y];
+        set => _image[x, y] = value;
+    }
+
+    public Rgb24ImageWrapper(Image<Rgb24> image)
+    {
+        _image = image;
+    }
+}
